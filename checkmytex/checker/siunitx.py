f05a42b834a63b765e03a88a1db7650c51cb2e96
Dominik Krupke <d-krupke@users.noreply.github.com>
2022-04-07 11:45:09 +0200

    simplified siunitx

--- checkmytex/checker/siunitx.py
+++ checkmytex/checker/siunitx.py
@@ -17,17 +17,14 @@ class SiUnitx(Checker):
         for match in self.regex.finditer(source):
             origin = document.get_origin_of_source(match.start("number"),
                                                    match.end("number"))
-            message = "It seems like you are using native numbers/units. " \
-                      "Prefer using `siunitx` with `\\num{123}` for numbers," \
-                      " `\\range{1}{3}` for ranges, and `\\SI{60}{\\second}`" \
-                      " for units. The syntax may have changed. Please check" \
-                      " the manual of `siunitx`. `siunitx` takes care of" \
-                      " proper spacing and uniform units."
+            message = "Use siunitx to get nice and uniform numbers and units."
+
             context = document.get_source_context(origin)
             rule = "SIUNITX"
             yield Problem(origin, message, context,
                           long_id=f"{rule}: {context}",
-                          rule=rule, tool="SIUNITX")
+                          rule=rule, tool="SIUNITX",
+                          look_up_url="https://ctan.org/pkg/siunitx?lang=en")
 
     def is_available(self) -> bool:
         return True
