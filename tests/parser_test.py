9595fb94f5c4a33041ca468605f43a602a6f3cc6
Dominik Krupke <d-krupke@users.noreply.github.com>
2023-06-07 22:11:09 +0200

    test

---
+++ tests/parser_test.py
@@ -0,0 +1,32 @@
+import unittest
+import flachtex
+from checkmytex.latex_document.parser import LatexParser
+
+class TestSource(unittest.TestCase):
+    def test_single_line(self):
+        files = {"main.tex": "0123456789"}
+        parser = LatexParser(flachtex.FileFinder(file_system=files))
+        sources = parser.parse_source("main.tex")
+        assert str(sources.flat_source) ==  files["main.tex"]
+        for i in range(10):
+            assert sources.investigate_origin(i).position.index == i
+            o = sources.get_simplified_origin_range(i, i + 1)
+            assert o[0].position.index == i
+            assert o[1].position.index == i + 1
+
+
+class TestLatexDocument(unittest.TestCase):
+    def test_single_line(self):
+        files = {"main.tex": "0123456789"}
+        parser = LatexParser(flachtex.FileFinder(file_system=files))
+        document = parser.parse("main.tex")
+        for i in range(10):
+            origin = document.get_simplified_origin_of_source(i, i + 1)
+            print(i, origin)
+            assert origin.begin.file.position.index == i
+            assert origin.end.file.position.index == i +1
+        for i in range(10):
+            origin = document.get_simplified_origin_of_text(i, i + 1)
+            print(i, origin)
+            assert origin.begin.file.position.index == i
+            assert origin.end.file.position.index == i +1 
